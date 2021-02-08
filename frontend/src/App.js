3e5847cce898fbdb88b1f5ef5798b52faedb2d47
Ilya Firsov <firsov1996@yandex.ru>
2021-02-08 19:45:56 +0100

    2.2 Starting the Frontend (Step 6) React-Bootstrap Setup, Header & Footer Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import logo from './logo.svg';
+import React from "react";
+import {Container} from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
-      My App
+      <Header />
+      <main className='py-4'>
+        <Container>
+          <h1>Welcome</h1>
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 }
