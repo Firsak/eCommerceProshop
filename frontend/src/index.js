e9b77e8a5b1689f02419fee6b9e5631a43b40b7b
Ilya Firsov <firsov1996@yandex.ru>
2021-02-11 22:08:19 +0100

    4.3 Product List Reducer & Action (Step 21) Implementing Redux For State Management

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import './bootstrap.min.css'
 import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 import store from "./store";
 import { Provider } from 'react-redux'
