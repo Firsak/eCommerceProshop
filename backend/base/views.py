460e6eb7ffe6b07475ba7d86bc846a95e20205ca
Ilya Firsov <firsov1996@yandex.ru>
2021-02-13 22:39:36 +0100

    6.5 User Serializer (Step 34) Backend User Authentication

--- backend/base/views.py
+++ backend/base/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from .models import Product
 from .products import products
 
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -18,10 +18,10 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
 
-        print(data)
+        serializer = UserSerializerWithToken(self.user).data
 
-        data['username'] = self.user.username
-        data['email'] = self.user.email
+        for k, v in serializer.items():
+            data[k] = v
 
         return data
 
@@ -55,4 +55,11 @@ def getProducts(request):
 def getProduct(request, pk):
     product = Product.objects.get(_id=pk)
     serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getUserProfile(request):
+    user = request.user
+    serializer = UserSerializer(user, many=False)
     return Response(serializer.data)
