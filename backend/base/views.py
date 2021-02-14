4d3c471a5a16d12e96e54ec917b2a205c21b9856
Ilya Firsov <firsov1996@yandex.ru>
2021-02-14 13:06:58 +0100

    6.7 Register User (Step 36) Backend User Authentication

--- backend/base/views.py
+++ backend/base/views.py
@@ -13,6 +13,9 @@ from .serializers import ProductSerializer, UserSerializer, UserSerializerWithTo
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
+
 # Create your views here.
 
 
@@ -32,18 +35,26 @@ class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
 
-@api_view(['GET'])
-def getRout(request):
-    routes = [
-        'api/products/',
-        'api/products/create/',
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
 
-        'api/products/upload/',
-        'api/products/<id>/',
+    print('DATA: ', data)
 
-    ]
-
-    return Response(routes)
+    try:
+        user = User.objects.create(
+            first_name=data['name'],
+            username=data['email'],
+            email=data['email'],
+            password=make_password(data['password']),
+        )
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    except:
+        message = {
+            'detail': 'User with this email already exists, or other error occurred'
+        }
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET'])
