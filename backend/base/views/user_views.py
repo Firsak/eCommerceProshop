8db105b1434b77813d4cdfc13f020d2ffbe1f475
Ilya Firsov <firsov1996@yandex.ru>
2021-02-14 18:46:57 +0100

    6.9 URL's & Views Cleanup (Step 38) Backend User Authentication

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -1,22 +1,15 @@
-from django.shortcuts import render
-from django.http import JsonResponse
 from django.contrib.auth.models import User
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
-from .models import Product
-from .products import products
-
-from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+from base.serializers import UserSerializer, UserSerializerWithToken
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from django.contrib.auth.hashers import make_password
 from rest_framework import status
-
-# Create your views here.
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -55,20 +48,6 @@ def registerUser(request):
             'detail': 'User with this email already exists, or other error occurred'
         }
         return Response(message, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['GET'])
-def getProducts(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def getProduct(request, pk):
-    product = Product.objects.get(_id=pk)
-    serializer = ProductSerializer(product, many=False)
-    return Response(serializer.data)
 
 
 @api_view(['GET'])
