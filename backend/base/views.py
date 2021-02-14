36bc0d14a3c36783d78bd6b00fa283c3e09f2861
Ilya Firsov <firsov1996@yandex.ru>
2021-02-14 12:40:35 +0100

    6.6 Protected Routes (Step 35) Backend User Authentication

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from django.contrib.auth.models import User
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
 from .models import Product
@@ -59,6 +61,15 @@ def getProduct(request, pk):
 
 
 @api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
