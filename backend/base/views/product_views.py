0ae88fd40e271d640fab3f82afa5f88eb2bb4949
Ilya Firsov <firsov1996@yandex.ru>
2021-02-23 13:59:50 +0100

    11.3 Create & Update Products Endpoints (Step 67) Admin Screen (Part 2)

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -17,10 +17,47 @@ def getProducts(request):
 
 
 @api_view(['GET'])
-@permission_classes([IsAdminUser])
 def getProduct(request, pk):
     try:
         product = Product.objects.get(_id=pk)
+        serializer = ProductSerializer(product, many=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    except:
+        message = {
+            'detail': "Product doesn't exists, or other error occurred"
+        }
+        return Response(message, status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def deleteProduct(request, pk):
+    try:
+        product = Product.objects.get(_id=pk)
+        product.delete()
+        return Response("Product deleted", status=status.HTTP_200_OK)
+    except:
+        message = {
+            'detail': "Some error occurred"
+        }
+        return Response(message, status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def createProduct(request):
+    try:
+        user = request.user
+        product = Product.objects.create(
+            user=user,
+            name="Sample name",
+            price=0,
+            brand="Sample brand",
+            countInStock=0,
+            category="Sample category",
+            description=""
+        )
+
         serializer = ProductSerializer(product, many=False)
         return Response(serializer.data, status=status.HTTP_200_OK)
     except:
@@ -30,12 +67,24 @@ def getProduct(request, pk):
         return Response(message, status=status.HTTP_404_NOT_FOUND)
 
 
-@api_view(['DELETE'])
-def deleteProduct(request, pk):
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def updateProduct(request, pk):
     try:
+        data = request.data
         product = Product.objects.get(_id=pk)
-        product.delete()
-        return Response("Product deleted", status=status.HTTP_200_OK)
+
+        product.name = data['name']
+        product.price = data['price']
+        product.brand = data['brand']
+        product.countInStock = data['countInStock']
+        product.category = data['category']
+        product.description = data['description']
+
+        product.save()
+
+        serializer = ProductSerializer(product, many=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     except:
         message = {
             'detail': "Product doesn't exists, or other error occurred"
