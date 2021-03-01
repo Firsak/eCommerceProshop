a31b0b74b76ba8ecce4dd29e24f1d661a63a5095
Ilya Firsov <firsov1996@yandex.ru>
2021-03-01 12:24:58 +0100

    13.2 Adding HashRouter (Step 80) Getting Ready For Deployment, Postgres & Going Live

--- backend/frontend/src/App.js
+++ backend/frontend/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import React from "react";
 import {Container} from "react-bootstrap";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {HashRouter as Router, Route} from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
