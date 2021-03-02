de1ecd1e433efdcfe5fc7071a76cb8508fe58d38
Ilya Firsov <firsov1996@yandex.ru>
2021-03-02 13:09:49 +0100

    13.3 Switching to Postgres Database (Step 81) Getting Ready For Deployment, Postgres & Going Live

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -121,10 +121,23 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+import backend.Password as Password
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ProShop',
+        'USER': 'postgres',
+        'PASSWORD': Password.PostgresQL,
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
